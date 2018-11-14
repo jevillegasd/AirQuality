0fc052881d1e2322c4ae91203c6797954c613d7c
Juan Villegas Delgado <jvillegasdelgado@minet.ae>
2018-11-14 16:29:02 +0400

    Code update and change in directories

--- Plotting/StackedPlots.R
+++ Plotting/StackedPlots.R
@@ -4,13 +4,13 @@
 ####                PM2.5 measuremnts. The two plots might differ if the samples include anormally high values.
 ####  Develloper:   Federico K., Khalifa Univeristy 2018
 ####                Juan Villegas. Khalifa Univeristy 2018
-####  Version:      1.1.0
-####  Date:         11/13/2018
+####  Version:      1.1.0.1
+####  Date:         11/14/2018
 ####  License:      CC 2018
 
 ####################### SETUP ####
 # Load packages
-library(threadr)
+#library(threadr)
 library(dygraphs)
 library(tidyr)
 library(leaflet)
@@ -18,10 +18,11 @@ library(readr)
 library(lubridate)
 library(ggplot2)
 library(scales)
+library(dplyr)
 
 #Set Work Environment: Make sure to set that with the dataset of interest.
-setwd("Z:/_SHARED_FOLDERS/Air Quality/Phase 2/PM Sampling (MoCCE)/2 Locations/01 Dubai Expo2020")
-fileName <-"PM_EXPO2020.csv"
+fileName <-"Datasets/PM_EXPO2020.csv"
+strLocation <- "Dubai Expo2020"
 
 #Usefull variables
 dust_event_t <- 1000    #treshold to identify something as a dust event and filter out the data
@@ -65,19 +66,21 @@ plot1 <- ggplot(mass_dataset, aes(as.POSIXct(Date), PM2.5)) +
   geom_point(aes(y = PM2.5, col = "PM2.5"), alpha=1, col="red", size = 4) +
   ylab(expression(paste(PM[2.5], " (µg/",m^3, ")", " monitored (24h)"))) + 
   theme(axis.title.x=element_blank(),
-        axis.text.x  = element_text(angle=90, vjust=0.5, hjust = 1, size=14, colour = "black", face="plain")) +
+        axis.text.x  = element_text(angle=90, vjust=0.5, hjust = 1, size=16, colour = "black", face="plain")) +
   theme(axis.title.y = element_text(face="plain", colour="black", size=16),
-        axis.text.y  = element_text(angle=0, vjust=0.5, size=14, colour = "black")) +
+        axis.text.y  = element_text(angle=0, vjust=-0.9, size=16, colour = "black")) +
   geom_hline(yintercept = 50, size = 1, linetype = "dashed")+
+  ggtitle(paste("Aerosol Particulate Matter(",strLocation,")")) + 
+  theme(plot.title = element_text(lineheight=.8, face="plain", size = 16, hjust = 0.5)) +
   scale_x_datetime(breaks = date_breaks("1 weeks"), labels = date_format("%d %b."))+
   ylim(0, 500)
 plot1
 
-fileDir <- paste0(outputDir,"timeSeries.png")
-png(fileDir,
-    width = 1700, height = 1050, units = "px", pointsize = 30,
+outputPath <- paste0(outputDir,"timeSeries.png")
+png(outputPath,
+    width = 1700, height = 900, units = "px", pointsize = 30,
     bg = "white", res = 150)
-print(plot)
+print(plot1)
 dev.off()
 
 # Include ouliers by removing the limits on the yscale
@@ -87,16 +90,18 @@ plot2 <- ggplot(mass_dataset, aes(as.POSIXct(Date), PM2.5)) +
   geom_point(aes(y = PM2.5, col = "PM2.5"), alpha=1, col="red", size = 4) +
   ylab(expression(paste(PM[2.5], " (µg/",m^3, ")", " monitored (24h)"))) + 
   theme(axis.title.x=element_blank(),
-        axis.text.x  = element_text(angle=90, vjust=0.5, hjust = 1, size=14, colour = "black", face="plain")) +
+        axis.text.x  = element_text(angle=90, vjust=0.5, hjust = 1, size=16, colour = "black", face="plain")) +
   theme(axis.title.y = element_text(face="plain", colour="black", size=16),
-        axis.text.y  = element_text(angle=0, vjust=0.5, size=14, colour = "black")) +
+        axis.text.y  = element_text(angle=0, vjust=-0.9, size=16, colour = "black")) +
   geom_hline(yintercept = 50, size = 1, linetype = "dashed")+
+  ggtitle(paste("Aerosol Particulate Matter(",strLocation,")")) + 
+  theme(plot.title = element_text(lineheight=.8, face="plain", size = 16, hjust = 0.5)) +
   scale_x_datetime(breaks = date_breaks("1 weeks"), labels = date_format("%d %b."))
 plot2
 
-fileDir <- paste0(outputDir,"timeSeries_ouliers.png")
-png(fileDir,
-    width = 1700, height = 1050, units = "px", pointsize = 30,
+outputPath <- paste0(outputDir,"timeSeries_ouliers.png")
+png(outputPath,
+    width = 1700, height = 900, units = "px", pointsize = 30,
     bg = "white", res = 150)
 print(plot2)
 dev.off()
@@ -123,16 +128,19 @@ plot3 <- ggplot(data = conc_dataset,
   ylab(expression(paste(PM[2.5], " (µg/",m^3, ")"),size=16)) + 
   theme(axis.title.y = element_text(face="plain", colour="black", size=16),
         axis.text.y  = element_text(angle=0, colour="black", vjust=0.5, size=16)) +
-  theme(axis.text.x  = element_text(angle=90, vjust=-0.8, hjust=1, size=16)) +
-  ggtitle("Chemical composition of fine Particulate Matter (EXPO2020 Dubai)") + 
-  theme(plot.title = element_text(lineheight=.8, face="plain", size = 16)) +
+  theme(axis.text.x  = element_text(angle=90, vjust=-0.9, hjust=1, size=16)) +
+  ggtitle(paste("Chemical composition of fine Particulate Matter (",strLocation,")")) + 
+  theme(plot.title = element_text(lineheight=.8, face="plain", size = 16, hjust = 0.5)) +
   ylim(0, 500) +
   scale_x_datetime(breaks = date_breaks("1 weeks"), labels = date_format("%d %b."))
 plot3
 
-fileDir <- paste0(outputDir,"stackedPlot.png")
-png(fileDir,
+outputPath <- paste0(outputDir,"stackedPlot.png")
+png(outputPath,
     width = 1700, height = 900, units = "px", pointsize = 13,
     bg = "white", res = 150)
 print(plot3)
 dev.off()
+
+#### END ####
+rm(list=ls())
