c1b4f6a7c3bb748f37f3ce501b08bd4c195dc569
jvillegas KU <juan.delgado@ku.ac.ae>
2018-11-29 17:49:22 +0400

    update on box script

--- Scripts/BoxWhiskers.R
+++ Scripts/BoxWhiskers.R
@@ -9,24 +9,8 @@
 ####################### SETUP ####
 #Set Work Environment: Make sure to set that with the dataset of interest.
 fileName <-"PM2.5 UAE.csv"
-strLocation <- "Dubai Ras Al Khor"
-strFilePrefix <- "AllBox"
-strDateInterval <- "1 days"
-
-# Load packages
-#library(threadr)
-library(dygraphs)
-library(tidyr)
-library(leaflet)
-library(readr)
-library(lubridate)
-library(ggplot2)
-library(scales)
-library(dplyr)
-
+strFilePrefix <- "PM2.5"
 fileDir = paste0(getwd(),"/Datasets/",fileName)
-#Usefull variables
-dust_event_t <- 1000    #treshold to identify something as a dust event and filter out the data
 
 ####################### IMPORT ALL PM CONCENTRATIONS DATASET ####
 
@@ -36,20 +20,15 @@ keeps <- c("Measurement", "Expo2020","Dubai.MoCCE","Ras.al.Khor","AD.MoCCE","Sha
 dataset <- dataset[keeps]           #Drop all the other variables
 str(dataset)                             #Displays the dataset properties
 
-boxplot(dataset$Expo2020, dataset$Dubai.MoCCE, dataset$Ras.al.Khor, dataset$AD.MoCCE,dataset$Sharjah,
-        main = "UAE PM2.5 field campaign",
-        at = c(1,2,3,4,5),
-        names = c("Dubai Expo2020", "Dubai MoCCAE", "Dubai Ras al Khor", "Abu Dhabi MoCCAE", "Sharjah"),
-        las = 1,
-        col = c("blue","green", "orange", "purple" ,"red"),
-        border = "black",
-        horizontal = TRUE,
-        notch = FALSE
-)
-
 dataset2 <- gather(dataset, "Location", "Concentration",2:6) 
 
-ggplot(dataset2, 
+####################### PLOTTING ####
+
+if (!(file.exists("Plots")))                  #Create a folder to store the files
+  dir.create(file.path(getwd(),"Plots"))
+outputDir <- paste0(getwd(),"/Plots/")
+
+plot1 <-ggplot(dataset2, 
         aes(Location, Concentration)) + 
         geom_boxplot() +
         ylab(expression(paste(PM[2.5], " (µg/",m^3, ") monitored (24h)")))+
@@ -59,27 +38,7 @@ ggplot(dataset2,
           axis.text.y  = element_text(angle=0, vjust=0, size=14, colour = "black")) +
         geom_hline(yintercept = 50, size = 1, linetype = "dashed")
 
-
-
-#######################   PM VALUES PLOT ####
-str("Generating Time Series Plots")  
-plot1 <- ggplot(mass_dataset, aes(as.POSIXct(Date), PM2.5)) + 
-  theme_bw() +
-  geom_line(aes(y = PM2.5, col = "PM2.5"), alpha=1, col="red", size = 1) +
-  geom_point(aes(y = PM2.5, col = "PM2.5"), alpha=1, col="red", size = 4) +
-  ylab(expression(paste(PM[2.5], "( µg/",m^3, ")", " monitored (24h)"))) + 
-  theme(axis.title.x=element_blank(),
-        axis.text.x  = element_text(angle=90, vjust=0.5, hjust = 1, size=16, colour = "black", face="plain")) +
-  theme(axis.title.y = element_text(face="plain", colour="black", size=16),
-        axis.text.y  = element_text(angle=0, vjust=-0.9, size=16, colour = "black")) +
-  geom_hline(yintercept = 50, size = 1, linetype = "dashed")+
-  ggtitle(paste("Aerosol Particulate Matter(",strLocation,")")) + 
-  theme(plot.title = element_text(lineheight=.8, face="plain", size = 16, hjust = 0.5)) +
-  scale_x_datetime(breaks = date_breaks(strDateInterval), labels = date_format("%d %b."))+
-  ylim(0, 300)
-plot1
-
-outputPath <- paste0(outputDir,strFilePrefix,"_TS.png")
+outputPath <- paste0(outputDir,strFilePrefix,"_Box.png")
 png(outputPath,
     width = 1700, height = 900, units = "px", pointsize = 30,
     bg = "white", res = 150)
